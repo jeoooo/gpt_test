0c31ab864c9c86c3063639b6b0ad4051056ce949
jeocarlolubao <jeocarlolubao@gmail.com>
2023-11-11 20:37:37 +0800

    added new tests

--- binary.py
+++ binary.py
@@ -1,19 +1,31 @@
+
+
+"""
+This script allows the user to interact with an AI chatbot using the g4f library. 
+The chatbot uses the GeekGpt provider and the gpt_35_turbo_16k_0613 model to generate responses based on the user's input. 
+The chat history is stored in the chat_hist list and is passed to the chatbot for context. 
+The script also enables logging and disables automatic version checking.
+
+"""
 import g4f
+
 
 g4f.debug.logging = True # enable logging
 g4f.check_version = False # Disable automatic version checking
+
 print(g4f.version) # check version
 print(g4f.Provider.Ails.params)  # supported args
 
-with open('prompts/tagalog_prompt.txt', 'r', encoding='UTF-8') as file:
+with open('prompts/binary_prompt.txt', 'r', encoding='UTF-8') as file:
     prompt_text = file.read()
+
+    print(prompt_text)
+
 
 
 chat_hist = [{"role": "system", "content": prompt_text}]
 
-# Automatic selection of provider
 print(chat_hist)
-
 # Automatic selection of provider
 while True:
     msg = input('\nEnter message: ')
